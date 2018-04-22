ab7cdb5f36e655b91cb2d244a9501a3f341fe4a3
Nikolaos Agas <agascompany@gmail.com>
2018-04-22 15:01:43 +0300

    056 tut

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Nikolaos Agas on 22/04/2018.
+//  Copyright © 2018 Nikolaos Agas. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitile: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
