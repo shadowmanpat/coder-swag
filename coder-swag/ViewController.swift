49c43f364dd5ed8d4b2f7707204e065938853e55
Nikolaos Agas <agascompany@gmail.com>
2018-04-22 14:36:06 +0300

    Initial Commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Nikolaos Agas on 22/04/2018.
+//  Copyright © 2018 Nikolaos Agas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
